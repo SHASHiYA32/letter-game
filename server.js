d93230318884d3be3c400c76fdcfcba18fe74f26
nimsara <nimsaraperera32@gmail.com>
2025-12-05 00:31:24 +0530

    First commit

--- server.js
+++ server.js
@@ -206,3 +206,13 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+const express = require("express");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
